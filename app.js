2ba7a46bb7e09a5508a52e01c9f7e559cba586eb
parelhutahaean <parel.hutahaean@gmail.com>
2017-04-20 21:43:41 +0700

    Edit Question

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/question');
+var tags = require('./routes/tags');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/tags', tags)
 
 
 // catch 404 and forward to error handler
--- routes/question.js
+++ routes/question.js
@@ -5,7 +5,8 @@ let models = require('../models')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   models.Question.findAll({
-    include : [models.User]
+    include : [models.User],
+    order: 'id ASC'
   })
   .then((query)=>{
     res.render('index',{
@@ -29,11 +30,11 @@ router.post('/question/submit',(req, res, next)=>{
     user_id : req.body.user_id
   })
   .then(()=>{
-    res.render('tags/tags',{})
+    res.render('tags/tags',{});
   })
 })
 
-router.get('question/delete/:id',(req,res,next)=>{
+router.get('/question/delete/:id',(req,res,next)=>{
   models.Question.destroy({
     where :{
       id : req.params.id
@@ -43,5 +44,55 @@ router.get('question/delete/:id',(req,res,next)=>{
     res.redirect('/')
   })
 })
+
+router.get('/question/edit/:id',(req,res,next)=>{
+  models.Question.find({
+    where : {
+      id : req.params.id
+    },
+    include : [models.User]
+  })
+  .then((query) => {
+    res.render('question/edit_question', {
+      data: query
+    });
+  })
+})
+
+router.post('/question/update/:id',(req, res, next)=>{
+  models.Question.update({
+      question: req.body.question,
+      user_id: req.body.user_id
+    },{
+    where : {
+      id : req.params.id
+    }
+  })
+  .then(data => {
+    res.redirect('/');
+  });
+})
+
+/*
+
+router.post('/update/:id', function(req, res, next) {
+  db.Todo.update({
+      task: req.body.task,
+      completed: req.body.completed || false,
+      user_id: req.body.user_id
+    },{
+    where : {
+      id : req.params.id
+    }
+  })
+  .then(data => {
+    res.redirect('/todos');
+  })
+  .catch(err => {
+    res.send(err);
+  })
+})
+
+*/
 
 module.exports = router;
